6b0c4defde4d7b85857c43ef6049fc91e6b218ee
Chermann-KING <codes.smiling@gmail.com>
2025-05-04 23:03:30 +0200

    🎨 feat : amélioration du formulaire de contact avec validation des champs, ajout d'informations de contact et mise à jour des traductions

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -1,14 +1,67 @@
+"use client";
+
+import { useTranslations } from "next-intl";
 import ContactHero from "@/components/sections/contact/ContactHero";
 import ContactForm from "@/components/ui/ContactForm";
 import Map from "@/components/ui/Map";
 import React from "react";
+import { Mail, MapPin, Phone } from "lucide-react";
 
 function Contact() {
+  const t = useTranslations("contact");
+
   return (
-    <div className="flex flex-col gap-16">
+    <div className="flex flex-col gap-16 pb-16">
       <ContactHero />
       <Map />
-      <ContactForm />
+      <div className="flex flex-col gap-8 px-4 md:px-0">
+        <h2
+          className="text-3xl tracking-tight font-extrabold text-center text-secondary-main sm:text-4xl md:text-5xl leading-none"
+          id="contact-form"
+        >
+          {t("getInTouch")}{" "}
+          <span className="text-primary-main">{t("getInTouchHighlight")}</span>
+        </h2>
+        <div className="flex-1 flex justify-center items-center">
+          <div className="w-full max-w-md text-center">
+            {Array.isArray(t.raw("description")) ? (
+              t.raw("description").map((text: string, index: number) => (
+                <p key={index} className="text-xl text-gray-600 mb-4">
+                  {text}
+                </p>
+              ))
+            ) : (
+              <p className="text-gray-600 mb-4">{t("description")}</p>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="max-w-7xl mx-auto flex justify-center items-start gap-8  border border-green-500">
+        <ContactForm />
+        {/* Contact Info */}
+        <div className="w-1/3 text-left">
+          <h3 className="text-lg font-semibold mb-4">{t("address.title")}</h3>
+          <div className="space-y-2">
+            <div className="flex items-center">
+              <MapPin className="w-5 h-5 mr-2" />
+              <span>{t("address.street")}</span>
+            </div>
+            <div className="flex items-center">
+              <Phone className="w-5 h-5 mr-2" />
+              <span>{t("address.phone")}</span>
+            </div>
+            <div className="flex items-center">
+              <Mail className="w-5 h-5 mr-2" />
+              <a
+                href="mailto:info@companyviene.com"
+                className="text-primary-main hover:underline"
+              >
+                <span>{t("address.email")}</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- components/ui/ContactForm.tsx
+++ components/ui/ContactForm.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { useTranslations } from "next-intl";
 import { useState } from "react";
 
 export default function ContactForm() {
+  const t = useTranslations("contact");
+
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -15,6 +18,7 @@ export default function ContactForm() {
   const [showErrors, setShowErrors] = useState(false);
   const [status, setStatus] = useState({
     type: "",
+    title: "",
     message: "",
   });
 
@@ -39,21 +43,21 @@ export default function ContactForm() {
     const newErrors: { [key: string]: string } = {};
 
     if (!formData.name) {
-      newErrors.name = "ne peut être vide";
+      newErrors.name = t("form.validation.required");
     }
 
     if (!formData.email) {
-      newErrors.email = "can't be empty";
+      newErrors.email = t("form.validation.required");
     } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-      newErrors.email = "ne peut être vide";
+      newErrors.email = t("form.validation.emailFormat");
     }
 
     if (!formData.subject) {
-      newErrors.subject = "ne peut être vide";
+      newErrors.subject = t("form.validation.required");
     }
 
     if (!formData.message) {
-      newErrors.message = "ne peut être vide";
+      newErrors.message = t("form.validation.required");
     }
 
     setErrors(newErrors);
@@ -68,7 +72,7 @@ export default function ContactForm() {
       return;
     }
 
-    setStatus({ type: "loading", message: "Envoi en cours..." });
+    setStatus({ type: "loading", title: "", message: t("form.submitting") });
 
     try {
       const response = await fetch("/api/contact", {
@@ -85,7 +89,8 @@ export default function ContactForm() {
 
       setStatus({
         type: "success",
-        message: "Message envoyé avec succès ! Nous vous contacterons bientôt.",
+        title: t("form.success.title"),
+        message: t("form.success.description"),
       });
       setFormData({
         name: "",
@@ -98,217 +103,217 @@ export default function ContactForm() {
     } catch (error) {
       setStatus({
         type: "error",
-        message: "Une erreur est survenue. Veuillez réessayer.",
+        title: t("form.error.title"),
+        message: t("form.error.description"),
       });
     }
   };
 
   return (
-    <section className="w-full mx-auto" id="contact-form">
-      <div className="w-full mx-auto py-12">
-        <form onSubmit={handleSubmit} className="max-w-2xl mx-auto">
-          {/* Nom complet */}
-          <div className="mb-6 relative">
-            <div className="flex justify-between">
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Nom complet
-              </label>
-              {showErrors && errors.name && (
-                <span className="text-xs font-semibold text-red-500">
-                  {errors.name}
-                </span>
-              )}
-            </div>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={formData.name}
-              onChange={handleChange}
-              className={`mt-1 block w-full rounded-md shadow-sm 
+    <section className="w-2/3 border border-blue-500">
+      <form onSubmit={handleSubmit}>
+        {/* Nom complet */}
+        <div className="mb-6 relative">
+          <div className="flex justify-between">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-700"
+            >
+              {t("form.name")}
+            </label>
+            {showErrors && errors.name && (
+              <span className="text-xs font-semibold text-red-500">
+                {errors.name}
+              </span>
+            )}
+          </div>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={formData.name}
+            onChange={handleChange}
+            className={`mt-1 block w-full rounded-md shadow-sm 
                 ${
                   showErrors && errors.name
                     ? "border-red-500 focus:border-red-500 focus:ring-red-500"
                     : "border-gray-300 focus:border-primary-main focus:ring-primary-main"
                 }`}
-            />
-            {showErrors && errors.name && formData.name === "" && (
-              <div className="absolute bg-orange-100 shadow-md p-2 rounded z-10 flex items-center">
-                <div className="bg-orange-500 text-white rounded-full w-6 h-6 flex items-center justify-center mr-2">
-                  !
-                </div>
-                Veuillez renseigner ce champ.
+          />
+          {showErrors && errors.name && formData.name === "" && (
+            <div className="absolute bg-orange-100 shadow-md p-2 rounded z-10 flex items-center">
+              <div className="bg-orange-500 text-white rounded-full w-6 h-6 flex items-center justify-center mr-2">
+                !
               </div>
+              {t("form.validation.name")}
+            </div>
+          )}
+        </div>
+
+        {/* Email */}
+        <div className="mb-6 relative">
+          <div className="flex justify-between">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              {t("form.email")}
+            </label>
+            {showErrors && errors.email && (
+              <span className="text-xs font-semibold text-red-500">
+                {errors.email}
+              </span>
             )}
           </div>
-
-          {/* Email */}
-          <div className="mb-6 relative">
-            <div className="flex justify-between">
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              {showErrors && errors.email && (
-                <span className="text-xs font-semibold text-red-500">
-                  {errors.email}
-                </span>
-              )}
-            </div>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="e.g. email@exemple.com"
-              className={`mt-1 block w-full rounded-md shadow-sm 
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="e.g. email@exemple.com"
+            className={`mt-1 block w-full rounded-md shadow-sm 
                 ${
                   showErrors && errors.email
                     ? "border-red-500 focus:border-red-500 focus:ring-red-500"
                     : "border-gray-300 focus:border-primary-main focus:ring-primary-main"
                 }`}
-            />
-            {showErrors && errors.email && formData.email === "" && (
-              <div className="absolute bg-orange-100 shadow-md p-2 rounded z-10 flex items-center">
-                <div className="bg-orange-500 text-white rounded-full w-6 h-6 flex items-center justify-center mr-2">
-                  !
-                </div>
-                Veuillez renseigner ce champ.
+          />
+          {showErrors && errors.email && formData.email === "" && (
+            <div className="absolute bg-orange-100 shadow-md p-2 rounded z-10 flex items-center">
+              <div className="bg-orange-500 text-white rounded-full w-6 h-6 flex items-center justify-center mr-2">
+                !
               </div>
-            )}
+              {t("form.validation.email")}
+            </div>
+          )}
+        </div>
+
+        {/* Téléphone et Sujet */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+          <div>
+            <label
+              htmlFor="phone"
+              className="block text-sm font-medium text-gray-700"
+            >
+              {t("form.phone")}
+            </label>
+            <input
+              type="tel"
+              name="phone"
+              id="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              placeholder="e.g. +241 77 88 38 52"
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-main focus:ring-primary-main"
+            />
           </div>
 
-          {/* Téléphone et Sujet */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
-            <div>
+          <div className="relative">
+            <div className="flex justify-between">
               <label
-                htmlFor="phone"
+                htmlFor="subject"
                 className="block text-sm font-medium text-gray-700"
               >
-                Téléphone
+                {t("form.subject")}
               </label>
-              <input
-                type="tel"
-                name="phone"
-                id="phone"
-                value={formData.phone}
-                onChange={handleChange}
-                placeholder="e.g. +33 6 12 34 56 78"
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-primary-main focus:ring-primary-main"
-              />
+              {showErrors && errors.subject && (
+                <span className="text-xs font-semibold text-red-500">
+                  {errors.subject}
+                </span>
+              )}
             </div>
-
-            <div className="relative">
-              <div className="flex justify-between">
-                <label
-                  htmlFor="subject"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Sujet
-                </label>
-                {showErrors && errors.subject && (
-                  <span className="text-xs font-semibold text-red-500">
-                    {errors.subject}
-                  </span>
-                )}
-              </div>
-              <input
-                type="text"
-                name="subject"
-                id="subject"
-                value={formData.subject}
-                onChange={handleChange}
-                className={`mt-1 block w-full rounded-md shadow-sm 
+            <input
+              type="text"
+              name="subject"
+              id="subject"
+              value={formData.subject}
+              onChange={handleChange}
+              className={`mt-1 block w-full rounded-md shadow-sm 
                   ${
                     showErrors && errors.subject
                       ? "border-red-500 focus:border-red-500 focus:ring-red-500"
                       : "border-gray-300 focus:border-primary-main focus:ring-primary-main"
                   }`}
-              />
-              {showErrors && errors.subject && formData.subject === "" && (
-                <div className="absolute bg-orange-100 shadow-md p-2 rounded z-10 flex items-center">
-                  <div className="bg-orange-500 text-white rounded-full w-6 h-6 flex items-center justify-center mr-2">
-                    !
-                  </div>
-                  Veuillez renseigner ce champ.
+            />
+            {showErrors && errors.subject && formData.subject === "" && (
+              <div className="absolute bg-orange-100 shadow-md p-2 rounded z-10 flex items-center">
+                <div className="bg-orange-500 text-white rounded-full w-6 h-6 flex items-center justify-center mr-2">
+                  !
                 </div>
-              )}
-            </div>
+                {t("form.validation.subject")}
+              </div>
+            )}
           </div>
+        </div>
 
-          {/* Message */}
-          <div className="mb-6 relative">
-            <div className="flex justify-between">
-              <label
-                htmlFor="message"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Message
-              </label>
-              {showErrors && errors.message && (
-                <span className="text-xs font-semibold text-red-500">
-                  {errors.message}
-                </span>
-              )}
-            </div>
-            <textarea
-              name="message"
-              id="message"
-              rows={4}
-              value={formData.message}
-              onChange={handleChange}
-              className={`mt-1 block w-full rounded-md shadow-sm 
+        {/* Message */}
+        <div className="mb-6 relative">
+          <div className="flex justify-between">
+            <label
+              htmlFor="message"
+              className="block text-sm font-medium text-gray-700"
+            >
+              {t("form.message")}
+            </label>
+            {showErrors && errors.message && (
+              <span className="text-xs font-semibold text-red-500">
+                {errors.message}
+              </span>
+            )}
+          </div>
+          <textarea
+            name="message"
+            id="message"
+            rows={4}
+            value={formData.message}
+            onChange={handleChange}
+            className={`mt-1 block w-full rounded-md shadow-sm 
                 ${
                   showErrors && errors.message
                     ? "border-red-500 focus:border-red-500 focus:ring-red-500"
                     : "border-gray-300 focus:border-primary-main focus:ring-primary-main"
                 }`}
-            />
-            {showErrors && errors.message && formData.message === "" && (
-              <div className="absolute bg-orange-100 shadow-md p-2 rounded z-10 flex items-center">
-                <div className="bg-orange-500 text-white rounded-full w-6 h-6 flex items-center justify-center mr-2">
-                  !
-                </div>
-                Veuillez renseigner ce champ.
+          />
+          {showErrors && errors.message && formData.message === "" && (
+            <div className="absolute bg-orange-100 shadow-md p-2 rounded z-10 flex items-center">
+              <div className="bg-orange-500 text-white rounded-full w-6 h-6 flex items-center justify-center mr-2">
+                !
               </div>
-            )}
-          </div>
-
-          {/* Message de statut */}
-          {status.message && (
-            <div
-              className={`p-4 rounded-md mb-6 ${
-                status.type === "success"
-                  ? "bg-green-50 text-green-800"
-                  : status.type === "error"
-                  ? "bg-red-50 text-red-800"
-                  : "bg-blue-50 text-blue-800"
-              }`}
-            >
-              {status.message}
+              {t("form.validation.message")}
             </div>
           )}
+        </div>
 
-          {/* Bouton d'envoi */}
-          <div className="flex justify-end">
-            <button
-              type="submit"
-              disabled={status.type === "loading"}
-              className="px-6 py-3 bg-green-600 hover:bg-green-700 text-white rounded-lg font-medium transition-colors duration-200"
-            >
-              {status.type === "loading"
-                ? "Envoi en cours..."
-                : "Envoyer le message"}
-            </button>
+        {/* Message de statut */}
+        {status.message && (
+          <div
+            className={`p-4 rounded-md mb-6 ${
+              status.type === "success"
+                ? "bg-green-50 text-green-800"
+                : status.type === "error"
+                ? "bg-red-50 text-red-800"
+                : "bg-blue-50 text-blue-800"
+            }`}
+          >
+            <span className="block font-medium">{status.title}</span>
+            <span>{status.message}</span>
           </div>
-        </form>
-      </div>
+        )}
+
+        {/* Bouton d'envoi */}
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            disabled={status.type === "loading"}
+            className="px-6 py-3 bg-green-600 hover:bg-green-700 text-white rounded-lg font-medium transition-colors duration-200"
+          >
+            {status.type === "loading"
+              ? `${t("form.submitting")}`
+              : `${t("form.submit")}`}
+          </button>
+        </div>
+      </form>
     </section>
   );
 }
